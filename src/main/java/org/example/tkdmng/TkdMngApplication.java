9e490ca983bff2eecd04a79ebf4368f670260491
Raul1963 <raulmarginean1963@gmail.com>
2025-03-18 11:26:46 +0200

    Initial

---
+++ src/main/java/org/example/tkdmng/TkdMngApplication.java
@@ -0,0 +1,13 @@
+package org.example.tkdmng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkdMngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TkdMngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/tkdmng/TkdMngApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tkdmng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TkdMngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
